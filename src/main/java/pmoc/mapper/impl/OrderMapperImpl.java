209ec7e4cde93dec833dfc6c26696ca3bef9f1ac
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-11-07 17:07:36 -0300

    feat: using mapper

--- src/main/java/pmoc/mapper/impl/OrderMapperImpl.java
+++ src/main/java/pmoc/mapper/impl/OrderMapperImpl.java
@@ -1,4 +1,25 @@
 package pmoc.mapper.impl;
 
-public class OrderMapperImpl {
+import org.springframework.stereotype.Component;
+import pmoc.DTOs.OrderDTO.RequestOrderDTO;
+import pmoc.DTOs.OrderDTO.ResponseOrderDTO;
+import pmoc.entities.OrderEntity;
+import pmoc.mapper.OrderMapper;
+
+@Component
+public class OrderMapperImpl implements OrderMapper {
+    @Override
+    public OrderEntity toEntity(RequestOrderDTO data) {
+        return null;
+    }
+
+    @Override
+    public ResponseOrderDTO toDTO(OrderEntity data) {
+        return null;
+    }
+
+    @Override
+    public OrderEntity partialUpdate(RequestOrderDTO dto, OrderEntity entity) {
+        return null;
+    }
 }
