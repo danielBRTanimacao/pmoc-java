07a4ef7ae069cfc53de75dddfbc10b0c6b050f54
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-11-13 14:49:32 -0300

    feat: created docker compose image

--- src/main/java/pmoc/entities/OrderEntity.java
+++ src/main/java/pmoc/entities/OrderEntity.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
